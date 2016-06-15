7a602a979f02226d5dd02e27f6e251ed1f602d07
Mike Dodge <mikedodge04@fb.com>
2016-06-14 19:16:51 -0700

    spelling in example

--- chef/cookbooks/cpe_init/recipes/company_init.rb
+++ chef/cookbooks/cpe_init/recipes/company_init.rb
@@ -1,5 +1,5 @@
 # This is where you set your own stuff...
 
 node.default['organization'] = 'MYCOMPANY'
-node.default['cpe_laucnhd']['prefix'] = 'com.MYCOMPANY.chef'
+node.default['cpe_launhd']['prefix'] = 'com.MYCOMPANY.chef'
 node.default['cpe_profiles']['prefix'] = 'com.MYCOMPANY.chef'
