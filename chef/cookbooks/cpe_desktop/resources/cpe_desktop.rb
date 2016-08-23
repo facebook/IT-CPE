8cfd3459c2d98c6cd121fa8bda8e1cd711e458a3
Chris Gerke <chris.gerke@gmail.com>
2016-08-23 13:53:29 +0800

    I really need to install that rubocop plugin ;)

--- chef/cookbooks/cpe_desktop/resources/cpe_desktop.rb
+++ chef/cookbooks/cpe_desktop/resources/cpe_desktop.rb
@@ -18,29 +18,29 @@ default_action :run
 action :run do
   prefs = node['cpe_desktop'].reject { |_k, v| v.nil? }
   return if prefs.empty?
-    prefix = node['cpe_profiles']['prefix']
-    organization = node['organization'] ? node['organization'] : 'Facebook'
-    override_picture_path = node['cpe_desktop']['override-picture-path']
-    node.default['cpe_profiles']["#{prefix}.desktop"] = {
-      'PayloadIdentifier' => "#{prefix}.desktop",
-      'PayloadRemovalDisallowed' => true,
-      'PayloadScope' => 'System',
-      'PayloadType' => 'Configuration',
-      'PayloadUUID' => '5450b6cb-83ef-56e5-65df-f3ba1af1ff6a',
-      'PayloadOrganization' => organization,
-      'PayloadVersion' => 1,
-      'PayloadDisplayName' => 'Desktop',
-      'PayloadContent' => [
-        {
-          'PayloadType' => 'com.apple.desktop',
-          'PayloadVersion' => 1,
-          'PayloadIdentifier' => "#{prefix}.desktop",
-          'PayloadUUID' => '4c390cb0-4832-0134-efbb-2c87a324f377',
-          'PayloadEnabled' => true,
-          'PayloadDisplayName' => 'Desktop',
-          'locked' => true,
-          'override-picture-path' => override_picture_path
-        }
-      ]
-    }
+  prefix = node['cpe_profiles']['prefix']
+  organization = node['organization'] ? node['organization'] : 'Facebook'
+  override_picture_path = node['cpe_desktop']['override-picture-path']
+  node.default['cpe_profiles']["#{prefix}.desktop"] = {
+    'PayloadIdentifier' => "#{prefix}.desktop",
+    'PayloadRemovalDisallowed' => true,
+    'PayloadScope' => 'System',
+    'PayloadType' => 'Configuration',
+    'PayloadUUID' => '5450b6cb-83ef-56e5-65df-f3ba1af1ff6a',
+    'PayloadOrganization' => organization,
+    'PayloadVersion' => 1,
+    'PayloadDisplayName' => 'Desktop',
+    'PayloadContent' => [
+      {
+        'PayloadType' => 'com.apple.desktop',
+        'PayloadVersion' => 1,
+        'PayloadIdentifier' => "#{prefix}.desktop",
+        'PayloadUUID' => '4c390cb0-4832-0134-efbb-2c87a324f377',
+        'PayloadEnabled' => true,
+        'PayloadDisplayName' => 'Desktop',
+        'locked' => true,
+        'override-picture-path' => override_picture_path
+      }
+    ]
+  }
 end
