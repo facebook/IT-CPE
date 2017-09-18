bbcbe8b5a22cae0add4c3c57f5a02e2088abfff1
Mike Dodge <mikedodge04@fb.com>
2017-09-18 14:03:13 -0700

    cpe_prompt_user

--- chef/cookbooks/cpe_prompt_user/resources/cpe_prompt_user.rb
+++ chef/cookbooks/cpe_prompt_user/resources/cpe_prompt_user.rb
@@ -15,45 +15,44 @@
 resource_name :cpe_prompt_user
 default_action :prompt
 
-
-# Required properties
-property :prompt_name, String, name_property: true
-property :message, String # informative-text
-# optional
-property :float, [ TrueClass, FalseClass ], default: true
-property :icon, String, default: node['cpe_prompt_user']['icon'] # icon-file
-property :interval, [ Integer, FalseClass ], default: false
-property :no_cancel, [ TrueClass, FalseClass ], default: true
-property :title, String, default: 'Facebook IT Says:' # title, text
-property :type, String, default: 'ok-msgbox' # type of prompt
-
-action :prompt do
-  phash = {
-    'program_arguments' => [],
-    'run_at_load' => true,
-    'type' => 'agent',
-    'limit_load_to_session_type' => 'Aqua',
-  }
-  cmd = [
-    node['cpe_prompt_user']['CocoaDialog'],
-    type,
-    '--title', title,
-    '--text', title,
-    '--icon-file', icon,
-  ]
-  ptype_adds = case type
-  when 'ok-msgbox'
-    [
-      '--informative-text', message,
+action_class do
+  def gen_prompt(p_name, ph)
+    icon = ph['icon'] ? ph['icon'] : node['cpe_prompt_user']['icon']
+    type = ph['type'] ? ph['type'] : 'ok-msgbox'
+    float = ph['float'] ? ph['float'] : true
+    phash = {
+      'program_arguments' => [],
+      'run_at_load' => true,
+      'type' => 'agent',
+      'limit_load_to_session_type' => 'Aqua',
+    }
+    cmd = [
+      node['cpe_prompt_user']['CocoaDialog'],
+      type,
+      '--title', ph['title'],
+      '--text', ph['title'],
+      '--icon-file', icon
     ]
+    ptype_adds = []
+    ptype_adds +=
+      case type
+      when 'ok-msgbox'
+        [
+          '--informative-text', ph['message']
+        ]
+      end
+    ptype_adds.each { |i| cmd << i }
+    cmd << '--float' if float
+    cmd << '--no-cancel' if ph['no_cancel']
+    # interval in mins
+    phash['start_interval'] = ph['interval'] * 60 if ph['interval']
+    phash['program_arguments'] = cmd
+    ld_prefix = node['cpe_launchd']['prefix']
+    p_name = p_name.tr(' ', '.')
+    node.default['cpe_launchd']["#{ld_prefix}.#{p_name}"] = phash
   end
-  ptype_adds.each { |i| cmd << i}
-  cmd << '--float' if float
-  cmd << '--no-cancel' if no_cancel
-  phash['start_interval'] = interval *60 if interval # interval in mins
-  phash['program_arguments'] = cmd
-  ld_prefix = node['cpe_launchd']['prefix']
-  prompt_name = name.gsub(' ', '.')
-  node.default['cpe_launchd']["#{ld_prefix}.#{prompt_name}"] = phash
 end
 
+action :prompt do
+  node['cpe_prompt_user']['prompts'].to_h.each { |k, v| gen_prompt(k, v) }
+end
