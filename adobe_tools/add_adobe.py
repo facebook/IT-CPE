5822bc94e8056ab6feb3dac657959c5a8b20f8d3
Nick McSpadden <nmcspadden@fb.com>
2017-09-26 16:37:17 -0700

    Refactor of adobe_tools
    Much cleaner and safer caching system

--- adobe_tools/add_adobe.py
+++ adobe_tools/add_adobe.py
@@ -3,6 +3,7 @@
 
 import sys
 
+import adobe_api
 import adobe_tools
 
 target_product = sys.argv[1]
@@ -18,30 +19,42 @@ def log(message):
   """Log with tag."""
   tag = 'CPE-add_adobe'
   print (tag + ': %s' % str(message))
+try:
+    # Do I exist as a user?
+    if not adobe_tools.user_exists(email):
+        log("Creating account for %s" % email)
+        # Add the user
+        success = adobe_tools.add_federated_user(
+            email,
+            email,
+            firstname,
+            lastname,
+            country
+        )
+        if not success:
+            log("Failed to create account for %s" % email)
+            sys.exit(1)
 
+    # Does the user already have the product?
+    log("Checking to see if %s already has %s" % (email, target_product))
+    already_have = adobe_tools.does_user_have_product(email, target_product)
+    if already_have:
+        log("User %s already has product %s" % (email, target_product))
+        sys.exit(0)
 
-# Do I exist as a user?
-if not adobe_tools.user_exists(email):
-  log("Creating account for %s" % email)
-  # Add the user
-  success = adobe_tools.add_federated_user(email, email, firstname,
-                                           lastname, country)
-  if not success:
-    log("Failed to create account for %s" % email)
-    sys.exit(1)
+    # Add desired product
+    log("Adding %s entitlement to %s" % (target_product, email))
+    result = adobe_tools.add_products([target_product], email)
+    if not result:
+        log("Failed to add product %s to %s" % (target_product, email))
+        sys.exit(1)
 
-# Does the user already have the product?
-log("Checking to see if %s already has %s" % (email, target_product))
-already_have = adobe_tools.does_user_have_product(email, target_product)
-if already_have:
-  log("User %s already has product %s" % (email, target_product))
-  sys.exit(0)
-
-# Add desired product
-log("Adding %s entitlement to %s" % (target_product, email))
-result = adobe_tools.add_products([target_product], email)
-if not result:
-  log("Failed to add product %s to %s" % (target_product, email))
-  sys.exit(1)
-
-log("Done.")
+    log("Done.")
+except adobe_api.AdobeAPIBadStatusException as e:
+    log("Encountered exception: %s" % e)
+    log(
+        "You were most likely rate limited - "
+        "this will automatically try again later. "
+        "Alternatively, please contact Help Desk."
+    )
+    exit(1)
--- adobe_tools/adobe_tools.py
+++ adobe_tools/adobe_tools.py
@@ -6,69 +6,84 @@ import adobe_api
 
 # These are the most common actions that one would use the Adobe UM API for.
 def user_exists(username):
-  """Return if the username exists."""
-  instance = adobe_api.AdobeAPIObject(username)
-  if instance.user:
+    """Return True if the username exists, or False if it doesn't."""
+    try:
+        adobe_api.AdobeAPIObject(username)
+    except adobe_api.AdobeAPINoUserException:
+        return False
     return True
-  return False
 
 
 def user_is_federated(username):
-  """
-  Return if the username exists and is federated.
+    """
+    Return if the username exists and is federated.
 
-  If the username does not exist, the result will be False.
-  """
-  instance = adobe_api.AdobeAPIObject(username)
-  if instance.user and instance.is_federated():
-    return True
-  return False
+    If the username does not exist, the result will be False.
+    """
+    try:
+        instance = adobe_api.AdobeAPIObject(username)
+    except adobe_api.AdobeAPINoUserException:
+        return False
+    return instance.is_federated()
 
 
 def does_user_have_product(product, username):
-  """Return True/False if a user has the specified product."""
-  instance = adobe_api.AdobeAPIObject(username)
-  return instance.has_product(product)
+    """Return True/False if a user has the specified product."""
+    try:
+        instance = adobe_api.AdobeAPIObject(username)
+    except adobe_api.AdobeAPINoUserException:
+        return False
+
+    return instance.has_product(product)
 
 
 def list_user_products(username):
-  """Return a list of the user's product configs."""
-  instance = adobe_api.AdobeAPIObject(username)
-  return instance.list_products()
+    """Return a list of the user's product configs."""
+    instance = adobe_api.AdobeAPIObject(username)
+    return instance.list_products()
 
 
 def does_product_exist(productname):
-  """Return True if a product config exists."""
-  instance = adobe_api.AdobeAPIObject("fake@fake.com")
-  return instance.product_exists(productname)
+    """Return True if a product config exists."""
+    instance = adobe_api.AdobeAPIObject(
+        "fake@fake.com",
+        allow_nonexistent_user=True
+    )
+    return instance.product_exists(productname)
 
 
 def get_product_list():
-  """Return a list of product configs available."""
-  instance = adobe_api.AdobeAPIObject("fake@fake.com")
-  productlist = instance.gather_product_list()
-  return [x['groupName'] for x in productlist]
+    """Return a list of product configs available."""
+    instance = adobe_api.AdobeAPIObject(
+        "fake@fake.com",
+        allow_nonexistent_user=True
+    )
+    productlist = instance.gather_product_list()
+    return [x['groupName'] for x in productlist]
 
 
 def add_federated_user(username, email, firstname, lastname, country='US'):
-  """Add federated user account."""
-  instance = adobe_api.AdobeAPIObject(username)
-  return instance.add_federated_user(email, country, firstname, lastname)
+    """Add federated user account."""
+    instance = adobe_api.AdobeAPIObject(
+        username,
+        allow_nonexistent_user=True
+    )
+    return instance.add_federated_user(email, country, firstname, lastname)
 
 
 def remove_user(username):
-  """Remove user account from organization."""
-  instance = adobe_api.AdobeAPIObject(username)
-  return instance.remove_user_from_org(username)
+    """Remove user account from organization."""
+    instance = adobe_api.AdobeAPIObject(username)
+    return instance.remove_user_from_org(username)
 
 
 def add_products(desired_products, username):
-  """Add products to specific user."""
-  instance = adobe_api.AdobeAPIObject(username)
-  return instance.add_products_to_user(desired_products)
+    """Add products to specific user."""
+    instance = adobe_api.AdobeAPIObject(username)
+    return instance.add_products_to_user(desired_products)
 
 
 def remove_products(removed_products, username):
-  """Remove products from specific user."""
-  instance = adobe_api.AdobeAPIObject(username)
-  return instance.remove_product_from_user(removed_products)
+    """Remove products from specific user."""
+    instance = adobe_api.AdobeAPIObject(username)
+    return instance.remove_product_from_user(removed_products)
