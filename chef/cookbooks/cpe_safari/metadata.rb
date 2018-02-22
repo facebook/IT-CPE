6d0a9c2c31b59b290e724e5fbe5bb697ad192502
Joseph Chilcote <chilcote+github@gmail.com>
2018-02-22 14:09:29 +0000

    updateing cpe_safari

--- chef/cookbooks/cpe_safari/metadata.rb
+++ chef/cookbooks/cpe_safari/metadata.rb
@@ -3,9 +3,9 @@
 name 'cpe_safari'
 maintainer 'Facebook'
 maintainer_email 'noreply@facebook.com'
-license 'BSD'
 description 'Manage and configure Safari browser.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.0.1'
 
+depends 'cpe_utils'
 depends 'cpe_profiles'
