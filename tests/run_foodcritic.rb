4c46e6b83300b4c2c7b6007200d67c003229b5d3
Johnny Ramos <johnnyramos@users.noreply.github.com>
2018-07-27 11:29:00 -0700

    Updating TravisCI to use RSpec for Linting (#184)
    * Create run_rubocop.rb
    
    Adding rspec rubocop file in which will run in Travis. This will determine if there was any changes compared to what is on master, then run the file against `rubocop`.
    
    * Delete travisci.rb
    
    * Delete run_linters.sh
    
    * Update and rename run_foodcritic to run_foodcritic.rb
    
    * Update .travis.yml
    
    * Rename run_robocop.rb to run_rubocop.rb
    
    * Update run_foodcritic.rb
    
    disabling rubocop for blocklength.
    
    * Update run_foodcritic.rb
    
    because travis told me so based on these tests.
    
    * Update run_foodcritic.rb

---
+++ tests/run_foodcritic.rb
@@ -0,0 +1,47 @@
+# rubocop:disable Metrics/BlockLength
+RSpec.describe 'Check that the cookbooks changed pass foodcritic.' do
+  before do
+    # Foodcritic rules to exclude.
+    @exclude_rules = %w[
+      FC014
+      FC017
+      FC024
+      FC059
+      FC064
+      FC065
+    ]
+    @exclude_rules = @exclude_rules.join(' -t ~')
+
+    # Diff-tree of only files in cookbooks
+    current_sha = 'origin/master..HEAD'
+    options = '--no-commit-id --diff-filter=ACMRT --name-only'
+    grep_cookbooks = 'grep chef/cookbooks'
+    @files =
+      `git diff-tree #{options} -r #{current_sha} | #{grep_cookbooks}`
+
+    @files = @files.split("\n")
+
+    @cookbooks = []
+    @files.each do |file|
+      dir = 'chef/cookbooks/' + file.split('/')[2]
+      @cookbooks << dir
+    end
+    @cookbooks = @cookbooks.uniq.join(' ')
+  end
+
+  it 'runs foodcritic on changed cookbooks' do
+    if @cookbooks.empty?
+      puts 'Foodcritic not performed. No cookbooks changed.'
+    else
+      puts 'Running foodcritic for changed cookbooks: ' +
+           @cookbooks
+      puts 'bundle exec foodcritic -f any -t ' +
+           "~#{@exclude_rules} #{@cookbooks}"
+      args = 'bundle exec foodcritic -f any -t ' +
+             "~#{@exclude_rules} #{@cookbooks}"
+      result = system args
+      expect(result).to be(true)
+    end
+  end
+end
+# rubocop:enable Metrics/BlockLength
---
+++ tests/run_rubocop.rb
@@ -0,0 +1,21 @@
+RSpec.describe 'Check that the files changed have correct syntax' do
+  before do
+    current_sha = 'origin/master..HEAD'
+    options = '--no-commit-id --diff-filter=ACMRT --name-only'
+    grep = 'grep .rb'
+    @files =
+      `git diff-tree #{options} -r #{current_sha} | #{grep}`
+    @files.tr!("\n", ' ')
+  end
+
+  it 'runs rubocop on changed ruby files' do
+    if @files.empty?
+      puts 'Linting not performed. No ruby files changed.'
+    else
+      puts "Running rubocop for changed files: #{@files}"
+      result = system \
+        "bundle exec rubocop --config .rubocop.yml #{@files}"
+      expect(result).to be(true)
+    end
+  end
+end
