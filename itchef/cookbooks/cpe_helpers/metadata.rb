9a38e2fd1b02677b22f1f4cc755733ae8d24004a
Michel Salim <michel@fb.com>
2019-06-28 15:21:00 -0700

    Update ShipIt Sync
    fbshipit-source-id: 4ab6344baeac5b6fe480f958dd91ff506e72bb58

--- itchef/cookbooks/cpe_helpers/metadata.rb
+++ itchef/cookbooks/cpe_helpers/metadata.rb
@@ -4,7 +4,7 @@ maintainer 'Facebook'
 maintainer_email 'noreply@facebook.com'
 license 'Apache-2.0'
 description 'Helper methods for Facebook IT-CPE open-source cookbooks'
-source_url 'https://github.com/facebook/IT-CPE/itchef/'
+source_url 'https://github.com/facebook/IT-CPE/tree/master/itchef/'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 # never EVER change this number, ever.
 version '0.1.0'
