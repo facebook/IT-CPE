1d48c19a74c3e51d61471bc0197589d04a8abaa4
Nate Walck <n8@uber.com>
2019-10-16 11:38:32 -0400

    Remove old dependency and add supports for debian family

--- itchef/cookbooks/cpe_hosts/metadata.rb
+++ itchef/cookbooks/cpe_hosts/metadata.rb
@@ -8,6 +8,7 @@ source_url 'https://github.com/facebook/IT-CPE/tree/master/itchef/'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.0'
 supports 'fedora'
+supports 'ubuntu'
+supports 'debian'
 supports 'mac_os_x'
 supports 'windows'
-depends 'line'
