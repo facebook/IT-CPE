61fe130812db1b236bb3107d5274b79078dd348c
Joseph Chilcote <chilcote@fb.com>
2020-02-10 14:24:53 -0800

    Re-sync with internal repository

---
+++ itchef/cookbooks/cpe_nomad/metadata.rb
@@ -0,0 +1,11 @@
+name 'cpe_nomad'
+maintainer 'Facebook'
+maintainer_email 'noreply@fb.com'
+license 'Apache-2.0'
+description 'Installs/configures NoMAD'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.0'
+
+depends 'cpe_launchd'
+depends 'cpe_logger'
+depends 'cpe_profiles'
