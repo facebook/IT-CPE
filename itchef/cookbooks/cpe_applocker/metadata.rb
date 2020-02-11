7898335a925e683764a407a1c8e74fb1fb788c8c
Nick Anderson <thor@fb.com>
2020-02-11 10:06:00 -0800

    applocker: updating maintainer info for oss
    Summary:
    
    This updates the maintainer information for the OSS push of the applocker recipe.
    
    Reviewed By: chilcote
    
    Differential Revision: D19820600
    
    fbshipit-source-id: 77b044059cf4feb2dbd7e13d01213282247fe0b9

--- itchef/cookbooks/cpe_applocker/metadata.rb
+++ itchef/cookbooks/cpe_applocker/metadata.rb
@@ -1,5 +1,5 @@
 name 'cpe_applocker'
-maintainer 'Client Protection Security Engineering'
+maintainer 'Facebook'
 maintainer_email 'noreply@fb.com'
 license 'Apache-2.0'
 description 'Installs and configures Applocker for Windows.'
