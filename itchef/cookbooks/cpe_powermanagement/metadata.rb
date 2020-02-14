1515b1b5fd62fd6bc24ae3e9ebe9f3d9c77bdc07
Michel Salim <michel@fb.com>
2020-02-13 16:32:29 -0800

    prep for open-sourcing
    Summary:
    
    This cookbook has previously been open sourced, and has diverged very little; ship the updated version.
    
    - headers updated
    - use `cpe_helpers` rather than `cpe_utils`
    
    Reviewed By: steelcowboy
    
    Differential Revision: D19887367
    
    fbshipit-source-id: 7882bf8af75631741ff9fb58a3a4a9d315236c49

--- itchef/cookbooks/cpe_powermanagement/metadata.rb
+++ itchef/cookbooks/cpe_powermanagement/metadata.rb
@@ -1,11 +1,11 @@
-
 name 'cpe_powermanagement'
 maintainer 'Facebook'
 maintainer_email 'noreply@facebook.com'
+license 'Apache-2.0'
 description 'Manages powermanagement settings / profile'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 supports 'mac_os_x'
 
-depends 'cpe_utils'
+depends 'cpe_helpers'
 depends 'cpe_profiles'
--- itchef/cookbooks/cpe_powermanagement/resources/cpe_powermanagement.rb
+++ itchef/cookbooks/cpe_powermanagement/resources/cpe_powermanagement.rb
@@ -1,13 +1,19 @@
-#
 # Cookbook Name:: cpe_powermanagement
 # Resource:: cpe_powermanagement
 #
-# Copyright (c) 2016-present, Facebook, Inc.
-# All rights reserved.
+# Copyright (c) Facebook, Inc. and its affiliates.
 #
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree. An additional grant
-# of patent rights can be found in the PATENTS file in the same directory.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 resource_name :cpe_powermanagement
@@ -22,9 +28,12 @@ action :config do
   end
 
   # Does the node have a battery?
-  machine_type = node.attr_lookup(
-    'hardware/battery', :default => []
-  ).empty? ? 'desktop' : 'portable'
+  machine_type =
+    if node['hardware'] && !node['hardware']['battery'].empty?
+      'portable'
+    else
+      'desktop'
+    end
   # Set the basic identifier
   ident = "com.apple.EnergySaver.#{machine_type}"
 
