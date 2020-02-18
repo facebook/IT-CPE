46335b5960c810b1a9ab374774bcd85d483984f1
Austin Culter <austinculter@gmail.com>
2020-02-18 17:54:32 +0100

    Rm `node.installed?('Google Chrome')` guard

--- chef/cookbooks/cpe_chrome/resources/cpe_chrome_win.rb
+++ chef/cookbooks/cpe_chrome/resources/cpe_chrome_win.rb
@@ -21,7 +21,7 @@ action :config do
   chrome_installed = ::File.file?(
     "#{ENV['ProgramFiles(x86)']}\\Google\\Chrome\\Application\\chrome.exe",
   )
-  return unless node.installed?('Google Chrome') || chrome_installed
+  return unless chrome_installed
   return unless node['cpe_chrome']['profile'].values.any?
 
   reg_settings = []
