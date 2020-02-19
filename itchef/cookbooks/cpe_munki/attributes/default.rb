6c7ef931f3f45320e4e15d7e7be4b4c600f0a93c
Michel Salim <michel@fb.com>
2020-02-18 18:01:18 -0800

    Re-sync with internal repository

---
+++ itchef/cookbooks/cpe_munki/attributes/default.rb
@@ -0,0 +1,76 @@
+#
+# Cookbook Name:: cpe_munki
+# Attributes:: default
+#
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+default['cpe_munki'] = {
+  'install' => false,
+  'configure' => false,
+  'auto_remediate' => nil,
+  'skip_enforcing_launchds' => [],
+  'munki_version_to_install' => {},
+  'local' => {
+    'managed_installs' => [],
+    'managed_uninstalls' => [],
+    'optional_installs' => [],
+  },
+  'preferences' => {
+    'AppleSoftwareUpdatesOnly' => nil,
+    'DaysBetweenNotifications' => nil,
+    'FollowHTTPRedirects' => nil,
+    'InstallAppleSoftwareUpdates' => nil,
+    'LogFile' => nil,
+    'LoggingLevel' => nil,
+    'LogToSyslog' => nil,
+    'PerformAuthRestarts' => nil,
+    'RecoveryKeyFile' => nil,
+    'SoftwareRepoURL' => nil,
+    'SuppressAutoInstall' => nil,
+    'SuppressStopButtonOnInstall' => nil,
+    'SuppressUserNotification' => nil,
+    'UnattendedAppleUpdates' => nil,
+    'UseClientCertificate' => nil,
+    'UseNotificationCenterDays' => nil,
+  },
+}
+
+# # Example of what to set in your company_init.rb
+# node.default['cpe_munki']['munki_version_to_install']['admin'] = {
+#   'version' => '3.0.0.3333',
+#   'checksum' =>
+#     '42fb19dbaa1d24691a596a3d60e900f57d2b9d6e1a8018972fe4c52c2f988682',
+# }
+# node.default['cpe_munki']['munki_version_to_install']['app'] = {
+#   'version' => '4.6.3330',
+#   'checksum' =>
+#   'f1354f99bececdabc0549531e50e1362a332a8e4802a07066e6bc0e74b72258d',
+# }
+# node.default['cpe_munki']['munki_version_to_install']['app_usage'] = {
+#   'version' => '3.0.0.3333',
+#   'checksum' =>
+#   'bc3299823d024982122de3d98905d28d6bf36585b060f7a0526a591c45815ad4',
+# }
+# node.default['cpe_munki']['munki_version_to_install']['core'] = {
+#   'version' => '3.0.0.3333',
+#   'checksum' =>
+#   'd82dd386d7aebe459314b7d62da993732e2b1e08813f305fab08ece10e2e330d',
+# }
+# node.default['cpe_munki']['munki_version_to_install']['launchd'] = {
+#   'version' => '3.0.3265',
+#   'checksum' =>
+#   'b3871f6bb3522ce5e46520bcab06aed2644bf11265653f6114a0e34911f17914',
+# }
---
+++ itchef/cookbooks/cpe_munki/resources/cpe_munki_local.rb
@@ -0,0 +1,148 @@
+#
+# Cookbook Name:: cpe_munki
+# Resource:: cpe_munki_local
+#
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'plist'
+require 'json'
+
+resource_name :cpe_munki_local
+default_action :run
+
+action :run do
+  locals_exist = node['cpe_munki']['local']['managed_installs'].any? ||
+                node['cpe_munki']['local']['managed_uninstalls'].any?
+
+  return unless locals_exist
+  return unless ::File.exist?('/usr/local/munki/managedsoftwareupdate')
+
+  # If local only manifest preference is not set,
+  # set it to the default 'extra_packages'
+  unless node['cpe_munki']['preferences'].key?('LocalOnlyManifest')
+    node.default['cpe_munki']['preferences']['LocalOnlyManifest'] =
+      'extra_packages'
+  end
+
+  @catalog_items = parse_items_in_catalogs
+
+  local_manifest = node['cpe_munki']['preferences']['LocalOnlyManifest']
+  file "/Library/Managed Installs/manifests/#{local_manifest}" do # ~FC005
+    owner 'root'
+    group 'wheel'
+    mode '644'
+    content gen_plist
+  end
+
+  file '/Library/Managed Installs/manifests/SelfServeManifest' do
+    not_if { node['cpe_munki']['local']['managed_uninstalls'].empty? }
+    only_if do
+      ::File.exist?('/Library/Managed Installs/manifests/SelfServeManifest')
+    end
+    owner 'root'
+    group 'wheel'
+    mode '644'
+    content gen_selfservice_plist
+  end
+
+  # This file is for context for users to know whats avalible for their machine
+  pretty_json = JSON.pretty_generate(@catalog_items)
+  file '/Library/Managed Installs/munki_catalog_items.json' do
+    owner 'root'
+    group 'wheel'
+    mode '644'
+    content pretty_json.to_s
+  end
+end
+
+def validate_install_array(install_array)
+  catalog_items = parse_items_in_catalogs
+  return install_array if catalog_items.empty?
+  ret = []
+  install_array.uniq.each do |item|
+    item_no_ver = item.split('-')[0] # strip version component of item
+    if catalog_items.include?(item_no_ver)
+      ret << item
+    else
+      ::Chef::Log.warn(
+        "#{cookbook_name}: item not found in catalogs: #{item}",
+      )
+    end
+  end
+  ret.uniq.sort
+end
+
+def gen_plist
+  installs = validate_install_array(
+    node['cpe_munki']['local']['managed_installs'],
+  )
+  uninstalls = validate_install_array(
+    node['cpe_munki']['local']['managed_uninstalls'],
+  )
+  optionals = validate_install_array(
+    node['cpe_munki']['local']['optional_installs'],
+  )
+  plist_hash = {
+    'managed_installs' => installs,
+    'managed_uninstalls' => uninstalls,
+    'optional_installs' => optionals,
+  }
+  ::Chef::Log.info("#{cookbook_name}: Managed installs: #{installs}")
+  unless optionals.empty?
+    ::Chef::Log.info("#{cookbook_name}: Optional installs: #{optionals}")
+  end
+  Plist::Emit.dump(plist_hash) unless plist_hash.nil?
+end
+
+def gen_selfservice_plist
+  self_serve_file = '/Library/Managed Installs/manifests/SelfServeManifest'
+  return nil unless ::File.exist?(self_serve_file)
+  ss_manifest = read_plist(self_serve_file)
+  uninstalls = validate_install_array(
+    node['cpe_munki']['local']['managed_uninstalls'],
+  )
+  # remove uninstalls from ss_manifest array
+  ss_manifest['managed_installs'] =
+    ss_manifest['managed_installs'] - uninstalls
+
+  ::Chef::Log.info(
+    "#{cookbook_name}: Cleanup Self Service: #{uninstalls}",
+  )
+  Plist::Emit.dump(ss_manifest)
+end
+
+def read_plist(xml_file)
+  Plist.parse_xml(xml_file)
+end
+
+def parse_items_in_catalogs
+  catalogs = []
+  catlogs_dir = '/Library/Managed Installs/catalogs/'
+  Dir.foreach(catlogs_dir) do |catalog|
+    next if ['.', '..'].include?(catalog)
+    begin
+      p = read_plist(catlogs_dir + catalog)
+      p.each do |d|
+        catalogs << d['name']
+      end
+    rescue StandardError
+      next
+    end
+  end
+  catalogs.uniq
+rescue StandardError
+  []
+end
