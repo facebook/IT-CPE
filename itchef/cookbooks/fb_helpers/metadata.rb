8c50a4c7fd43a3282bf98e2934ddcf29089774b1
Austin Culter <austin.culter@uber.com>
2021-10-15 08:57:24 +0200

    Add `supports mac_os_x` to `fb_helpers/metadata.rb`
    - Add `supports mac_os_x` to `fb_helpers/metadata.rb`
    - Alphabetize `supports` list

--- itchef/cookbooks/fb_helpers/metadata.rb
+++ itchef/cookbooks/fb_helpers/metadata.rb
@@ -10,5 +10,6 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 supports 'centos'
 supports 'debian'
-supports 'ubuntu'
 supports 'freebsd'
+supports 'mac_os_x'
+supports 'ubuntu'
