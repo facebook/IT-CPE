d8c9c9bca2f202a3c00d1d23af4cd17604a23041
Alexey Savva <alexeys@meta.com>
2025-09-26 14:10:15 -0700

    Remove long_description vol.10
    Differential Revision: D83104772
    
    fbshipit-source-id: f8387af6c06a9e499c0009794304c233fec06e70

--- itchef/cookbooks/cpe_kernel_channel/metadata.rb
+++ itchef/cookbooks/cpe_kernel_channel/metadata.rb
@@ -5,6 +5,5 @@ maintainer_email 'noreply@facebook.com'
 license 'Apache-2.0'
 description 'Configures the repo used to deliver kernel packages'
 source_url 'https://github.com/facebook/IT-CPE/tree/master/itchef/'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.0'
 depends 'fb_helpers'
