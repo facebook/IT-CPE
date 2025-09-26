05bf0934ecdb86bc1d23b4c3e30e1bd256de1f27
Alexey Savva <alexeys@meta.com>
2025-09-26 14:59:49 -0700

    Final cleanup and linters tune up
    Differential Revision: D83381076
    
    fbshipit-source-id: a88a2e97156b6ae5153364539cebbbf62275c3ef

--- itchef/cookbooks/cpe_user_customizations/metadata.rb
+++ itchef/cookbooks/cpe_user_customizations/metadata.rb
@@ -4,7 +4,6 @@ maintainer 'Facebook'
 maintainer_email 'noreply@facebook.com'
 license 'Apache-2.0'
 description 'This cookbook holds recipes each person creates for themselves.'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.0.1'
 
 depends 'cpe_helpers'
