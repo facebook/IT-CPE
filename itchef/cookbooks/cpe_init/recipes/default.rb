fc9a1445c3a964db8655a91d4b9f1a6b01ef3afb
Joseph Chilcote <chilcote@meta.com>
2026-03-23 15:45:25 -0700

    Remove unused cpe_preferencepanes
    Reviewed By: gbatye
    
    Differential Revision: D97673061
    
    fbshipit-source-id: e9da75242dcb629d2fa19e534822b278eca91e8b

--- itchef/cookbooks/cpe_init/recipes/default.rb
+++ itchef/cookbooks/cpe_init/recipes/default.rb
@@ -46,7 +46,6 @@ if node.macos?
     'cpe_deprecation_notifier',
     'cpe_munki',
     'cpe_powermanagement',
-    'cpe_preferencepanes',
     'cpe_spotlight',
     # Here Be Dragons... Ordering is important.
     # launchd and profiles need to be last, as other apis depend on these
