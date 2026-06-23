25fceff1f786f957b0a3dd84087f76d6a229ca4e
Marlon Jacques <marjac@meta.com>
2026-06-23 15:43:42 -0700

    Delete cpe_windows_update_for_business cookbook
    Reviewed By: anps77
    
    Differential Revision: D107280544
    
    fbshipit-source-id: 7df270a4fb1eaeeead7f46a09a169ecce50b196b

--- itchef/cookbooks/cpe_windows_update_for_business/metadata.rb
+++
@@ -1,9 +0,0 @@
-name 'cpe_windows_update_for_business'
-maintainer 'Facebook'
-maintainer_email 'noreply@facebook.com'
-license 'Apache-2.0'
-description 'Configures cpe_windows_update_for_business'
-version '0.1.0'
-supports 'windows'
-
-depends 'cpe_helpers'
